fd213be0d26611425c5fc885099c9660a044d2f2
Ismael bienvenu <ismaellongouate@gmail.com>
2024-12-21 13:15:21 +0100

    added

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- Ajouter le meta tag CSRF -->
     <title>Scan & MRZ</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
